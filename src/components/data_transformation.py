10017f5ec82214624c2c7dab2e885127d343ac53
goodnessohi <76630943+goodnessohi@users.noreply.github.com>
2024-07-16 20:46:32 +0100

    updated data_ingestion

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -17,7 +17,11 @@ from src.utils import save_obj
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
 
-def get_data_transformer_object(self):
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config= DataTransformationConfig()
+
+    def get_data_transformer_object(self):
         '''
         This function is responsible for data transformation
         '''
@@ -55,7 +59,7 @@ def get_data_transformer_object(self):
         except Exception as e:
             raise CustomException(e,sys)
         
-def initiate_data_transformation(self, train_path, test_path):
+    def initiate_data_transformation(self, train_path, test_path):
 
         try:
             train_df = pd.read_csv(train_path)
@@ -74,6 +78,9 @@ def initiate_data_transformation(self, train_path, test_path):
             target_feature_test_df = test_df[target_column_name]
             target_feature_train_df = train_df[target_column_name]
             logging.info(f'Applying preprocessing object on training dataframe and testing datatframe')
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
@@ -96,11 +103,5 @@ def initiate_data_transformation(self, train_path, test_path):
             )
 
         except Exception as e:
-            raise CustomException(str(e))
+            raise CustomException(str(e), sys)
         
-if __name__ == '__main__':
-    obj = DataTransformation()
-    train_path = 'path_to_train_file.csv'  # replace with your train file path
-    test_path = 'path_to_test_file.csv'  # replace with your test file path
-    transformed_data = obj.initiate_data_transformation(train_path, test_path)
-    print(transformed_data)
