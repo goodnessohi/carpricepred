fa3022a9e7ba703a7fba5d77bdb8ac682fbbd93b
goodnessohi <76630943+goodnessohi@users.noreply.github.com>
2024-07-21 19:25:37 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -17,3 +17,17 @@ def save_obj(file_path, obj):
     except Exception as e:
         print(f"Error: {str(e)}")
         raise CustomException(str(e))
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    report = {}
+    for model_name, model in models.items():
+        try:
+            model.fit(X_train, y_train)  # Train model
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[model_name] = test_model_score
+        except Exception as e:
+            print(f"Error evaluating model {model_name}: {str(e)}")
+    return report
