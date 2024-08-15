2a327fc1d8f216bcbb8e05eb56fa18d8d5de075f
goodnessohi <76630943+goodnessohi@users.noreply.github.com>
2024-08-15 09:28:14 +0100

    Improced Model training, Created predicted pipeline, and requirements.txt

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from src.pipeline.predict_pipeline import PredictPipeline, CustomData  # Import your classes
+
+# Create a title
+st.title("Car Price Prediction App")
+
+# Create input fields
+year = st.number_input("Year", min_value=1900, max_value=2024)
+km_driven = st.number_input("Kilometers Driven", min_value=0)
+mileage = st.number_input("Mileage", min_value=0.0)
+engine = st.number_input("Engine", min_value=0.0)
+seats = st.number_input("Seats", min_value=1)
+max_power = st.number_input("Max Power", min_value=0.0)
+fuel = st.selectbox("Fuel", ['Diesel', 'Petrol', 'LPG', 'CNG'])
+seller_type = st.selectbox("Seller Type", ['Individual', 'Dealer', 'Trustmark Dealer'])
+transmission = st.selectbox("Transmission", ['Manual', 'Automatic'])
+owner = st.selectbox("Owner", ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'])
+brand = st.selectbox("Brand", ['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 
+                               'Ford', 'Renault', 'Mahindra', 'Tata', 'Chevrolet', 
+                               'Fiat', 'Datsun', 'Jeep', 'Mercedes-Benz', 'Mitsubishi', 
+                               'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus', 'Jaguar', 
+                               'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Force', 'Ambassador', 
+                               'Ashok', 'Isuzu', 'Opel', 'Peugeot'])
+
+# Create a button to submit inputs
+if st.button("Submit"):
+    # Create a CustomData object with the inputs
+    data = CustomData(
+        year=year,
+        km_driven=km_driven,
+        mileage=mileage,
+        engine=engine,
+        seats=seats,
+        max_power=max_power,
+        fuel=fuel,
+        seller_type=seller_type,
+        transmission=transmission,
+        owner=owner,
+        brand=brand
+    )
+    
+    # Convert CustomData to DataFrame
+    df = data.get_data_as_df()
+    
+    # Create a PredictPipeline object
+    pipeline = PredictPipeline()
+    
+    # Create a progress bar
+    with st.spinner('Computing prediction...'):
+    # Get the prediction
+        prediction = pipeline.predict(df)
+
+    # Truncate decimal points and add dollar sign
+        predicted_price = "${:,.2f}".format(prediction[0])
+
+    
+    # Display the inputs and prediction
+    st.write("Inputs:")
+    st.write(df)
+    st.write("Prediction:")
+    st.write(predicted_price)
+
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,74 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+from src.logger import logging
+import os
+
+class PredictPipeline:
+    def __init__(self):
+        
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = os.path.join("artifacts", "model.pkl")
+            preprocessor_path = os.path.join("artifacts","preprocessor.pkl")
+            logging.info('Model and preprocessor path obtained')
+            
+            model = load_object(file_path=model_path)    
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+class CustomData:
+    def __init__(self,
+                year: int,
+                km_driven: int,
+                mileage: float,
+                engine: float,
+                seats: int,
+                max_power: float,
+                fuel: str,
+                seller_type: str,
+                transmission: str,
+                owner: str,
+                brand: str
+                 ):
+        self.year = year
+        self.km_driven = km_driven
+        self.mileage = mileage
+        self.engine = engine
+        self.seats = seats
+        self.max_power = max_power
+        self.fuel = fuel
+        self.seller_type = seller_type
+        self.transmission = transmission
+        self.owner = owner
+        self.brand = brand
+
+    def get_data_as_df(self):
+        try:
+            custom_data_input_dict = {
+            "year": [self.year],
+            "km_driven": [self.km_driven],
+            "mileage": [self.mileage],
+            "engine": [self.engine],
+            "seats": [self.seats],
+            "max_power": [self.max_power],
+            "fuel": [self.fuel],
+            "seller_type": [self.seller_type],
+            "transmission": [self.transmission],
+            "owner": [self.owner],
+            "brand": [self.brand],
+        }
+
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+                raise CustomException(e, sys)
+
